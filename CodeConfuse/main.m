ecf5a615802733bd3c0f4cccc8b7e6b0df7110e5
EShow <yinsong.xue@wkzuche.com>
2019-08-15 11:57:11 +0800

    Initial Commit

---
+++ CodeConfuse/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CodeConfuse
+//
+//  Created by eShow on 2019/8/15.
+//  Copyright © 2019 jiaoyingbrother. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
